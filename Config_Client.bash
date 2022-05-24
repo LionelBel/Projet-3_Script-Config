87a5a68f17a458da6b6573555d7bdcdc051c39c6
Lionel <lionel.bellerose@outlook.com>
2022-05-24 12:36:53 +0200

    Rédaction entête script de configuration poste client.

---
+++ Config_Client.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+################################################                                              
+#                                              #
+#          Script de Configuration             #      
+#         des Postes clients UBUNTU            #
+#                                              #
+#                     A                        #
+#                 Fond Linux                   #
+#                                              #
+# Lionel BELLEROSE                             #
+#                                              #
+################################################
